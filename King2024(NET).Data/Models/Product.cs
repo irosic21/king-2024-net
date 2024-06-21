a131989d23d35c1059557c561120dfb98d075762
irosic21 <rosicivo12@gmail.com>
2024-06-21 16:41:33 +0200

    Created data model for products and removed redundant class Class1.cs

---
+++ King2024(NET).Data/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King2024_NET_.Data.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ShortDescription { get; set; }
+    }
+}
