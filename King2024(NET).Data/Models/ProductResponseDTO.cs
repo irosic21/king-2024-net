42ca10703f72017129dc2421876001eabbfeda2d
irosic21 <rosicivo12@gmail.com>
2024-06-23 18:01:42 +0200

    Created additional Models, ProductDTO and ProductResponseDTO

---
+++ King2024(NET).Data/Models/ProductResponseDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King2024_NET_.Data.Models
+{
+    public class ProductResponseDTO
+    {
+        public List<ProductDTO> Products { get; set; }
+        public int Total { get; set; }
+        public int Skip { get; set; }
+        public int Limit { get; set; }
+    }
+}
