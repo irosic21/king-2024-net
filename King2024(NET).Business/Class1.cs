68f5a69f4363a0e74a1d22f51c4e3dbbe2c574f0
irosic21 <rosicivo12@gmail.com>
2024-06-23 18:15:50 +0200

    I added the interface IProductServices with all needed methods, removed the Class1 file, and refactored the Products file so that its class is public

--- King2024(NET).Business/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace King2024_NET_.Business
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ King2024(NET).Business/Interfaces/IProductServices.cs
@@ -0,0 +1,17 @@
+﻿using King2024_NET_.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King2024_NET_.Business.Interfaces
+{
+    public interface IProductServices
+    {
+        public Task<List<Product>> GetAllProductsAsync();
+        public Task<Product> GetProductByIdAsync(int id);
+        public Task<List<Product>> SearchProductsAsync(string searchText);
+        public Task<List<Product>> FilterProductsAsync(string category, decimal? minPrice, decimal? maxPrice);
+    }
+}
